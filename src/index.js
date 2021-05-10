29c95a50ed4125c092cfec7486a083e84eb8d633
Otilia Visan <otilia.visan22@gmail.com>
2021-05-11 00:49:24 +0100

    prompt and validate the first questions

--- src/index.js
+++ src/index.js
@@ -7,22 +7,47 @@ const inquirer = require("inquirer");
 //Questions
 
 const questions = [
+  return inquirer.prompt([
   {
     type: "input",
     name: "name",
     message: "What's the name of your employee?",
+    validate: nameInput => {
+      if(nameInput){
+        return true;
+      } else {
+        console.log("Please let me know the name of your employee")
+        return false;
+      } 
+    }
   },
 
   {
     type: "input",
     name: "id",
     message: "What's the id of your employee?",
+    validate: nameId => {
+      if(nameId){
+        return true;
+      } else {
+        console.log("Please let me know the id of your employee")
+        return false;
+      } 
   },
 
   {
     type: "input",
     name: "email",
     message: "What's the email address of your employee?",
+    validate: email => {
+valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+    if (valid) {
+      return true;
+    } else {
+      console.log ("Please enter your email address");
+      return false;
+    } 
+  } 
   },
 
   {
@@ -31,4 +56,54 @@ const questions = [
     message: "What's the role of your employee?",
     choices: ["manager", "intern", "engineer"],
   },
+  )];
 ];
+
+// Manager question
+const managerQuestion = [
+  return inquirer.prompt([
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What's the manager office number?",
+    validate: 
+  },
+  )];
+];
+
+//Engineer question
+const engineerQuestion = [
+  {
+    type: "input",
+    name: "username",
+    message: "What's the github user of the engineer office?",
+  },
+];
+
+//Intern question
+const internQuestion = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's the school name of the intern?",
+  },
+];
+
+//In the case you need to add another employee
+const anotherQuestion = [
+  {
+    type: "list",
+    name: "name",
+    message: "Would you like to add another employee?",
+    choices: ["Yes", "No"],
+  },
+];
+
+//Create a function to blend together the data
+
+//Create a function to initialize the app
+async () => {
+  const answers = await inquirer.prompt(questions).then(function(response));
+  return employeeAnswers = response;
+
+};
