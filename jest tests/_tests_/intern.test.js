a69eceea454267dcb5989824153720021f6d4eeb
Otilia Visan <otilia.visan22@gmail.com>
2021-05-11 17:11:42 +0100

    rendered the tests for intern class

---
+++ jest tests/_tests_/intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/classes/intern");
+
+test("Can the school be defined with constructor?", () => {
+  const testValue = "Stavanger School";
+  const e = new Intern("Foo", 1, test@test.com, testValue);
+  expect(typeof e.school).toBe("testValue");
+});
+
+test("getRole should return  \"Intern"\" => {
+  const testValue = Intern;
+  const e = new Intern("Foo",1,test@test.com,"Stavanger School");
+  expect(typeof e.getRole()).toBe("testValue");
+});
+
+test("Can get school username via getSchool()?", () => {
+  const testValue = "Stavanger School";
+  const e = new Intern("Foo", 1, test@test.com, testValue);
+  expect(typeof e.GetSchool()).toBe("testValue");
+});
