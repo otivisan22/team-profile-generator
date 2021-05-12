bdc210c233eee1ecc62a04ed51f25cc297114e78
Otilia Visan <otilia.visan22@gmail.com>
2021-05-12 10:56:05 +0100

    corrected the errors on classes and test upon employee class

--- jest tests/_tests_/employee.test.js
+++ jest tests/_tests_/employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require("../lib/Classes/employee");
+const Employee = require("../../lib/classes/employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
@@ -6,45 +6,43 @@ test("Can instantiate Employee instance", () => {
 });
 
 test("Can set name via constructor logic?", () => {
-  const name = Hampus;
+  const name = "Hampus";
   const e = new Employee(name);
-  expect(typeof e.name).toBe("name");
+  expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor logic?", () => {
   const testValue = 200;
-  const e = new Employee("Foo",testValue);
-  expect(typeof e.id).toBe("testValue");
+  const e = new Employee("Foo", testValue);
+  expect(e.id).toBe("testValue");
 });
 
 test("Can set email address via constructor logic?", () => {
-  const testValue = test@test.com
+  const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
-  expect(typeof e.email).toBe("testValue");
+  expect(e.email).toBe(testValue);
 });
 
 test("Can getName via constructor logic?", () => {
-  const testValue = "Hampus"
+  const testValue = "Hampus";
   const e = new Employee(testValue);
-  expect(typeof e.getName()).toBe("testValue");
+  expect(e.getName()).toBe(testValue);
 });
-
 
 test("Can getId via constructor logic?", () => {
-  const testValue = 200;
-  const e = new Employee("Foo",testValue);
-  expect(typeof e.getiD()).toBe("testValue");
+  const testValue = "200";
+  const e = new Employee("Foo", testValue);
+  expect(e.getiD()).toBe(testValue);
 });
-
 
 test("Can getEmail via constructor logic?", () => {
-  const testValue = test@test.com
-  const e = new Employee("Foo",1,testValue);
-  expect(typeof e.getEmail()).toBe("testValue");
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.getEmail()).toBe(testValue);
 });
 
-test("getRole should return  \"Employee"\" => {
-  const testValue = Employee;
-  const e = new Employee("Hampus",1,test@test.com);
-  expect(typeof e.getRole()).toBe("testValue");
+test('getRole should return "Employee"', () => {
+  const testValue = "Employee";
+  const e = new Employee("Hampus", 1, "test@test.com");
+  expect(e.getRole()).toBe(testValue);
 });
--- src/index.js
+++ src/index.js
@@ -8,111 +8,115 @@ const inquirer = require("inquirer");
 
 const questions = () => {
   return inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What's the name of your employee?",
-    validate: nameInput => {
-      if(nameInput){
-        return true;
-      } else {
-        console.log("Please let me know the name of your employee")
-        return false;
-      } 
-    }
-  },
+    {
+      type: "input",
+      name: "name",
+      message: "What's the name of your employee?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please let me know the name of your employee");
+          return false;
+        }
+      },
+    },
 
-  {
-    type: "input",
-    name: "id",
-    message: "What's the id of your employee?",
-    validate: nameId => {
-      if(nameId){
-        return true;
-      } else {
-        console.log("Please let me know the id of your employee")
-        return false;
-      } 
-  },
+    {
+      type: "input",
+      name: "id",
+      message: "What's the id of your employee?",
+      validate: (nameId) => {
+        if (nameId) {
+          return true;
+        } else {
+          console.log("Please let me know the id of your employee");
+          return false;
+        }
+      },
+    },
 
-  {
-    type: "input",
-    name: "email",
-    message: "What's the email address of your employee?",
-    validate: email => {
-valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
-    if (valid) {
-      return true;
-    } else {
-      console.log ("Please enter your email address");
-      return false;
-    } 
-  } 
-  },
+    {
+      type: "input",
+      name: "email",
+      message: "What's the email address of your employee?",
+      validate: (email) => {
+        valid =
+          /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+            email
+          );
+        if (valid) {
+          return true;
+        } else {
+          console.log("Please enter your email address");
+          return false;
+        }
+      },
+    },
 
-  {
-    type: "input",
-    name: "list",
-    message: "What's the role of your employee?",
-    choices: ["manager", "intern", "engineer"],
-  },
-  )];
-  };
+    {
+      type: "input",
+      name: "list",
+      message: "What's the role of your employee?",
+      choices: ["manager", "intern", "engineer"],
+    },
+  ]);
+};
 
 // Manager question
 const managerQuestion = () => {
   return inquirer.prompt([
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What's the manager office number?",
-    validate: nameInput =>
     {
-      if(isNaN(nameInput)){
-      return false;
-      }else {
-        console.log ("Please provide the office number");
-        return true;
-      }
-    }
-  },
-  )];
+      type: "input",
+      name: "officeNumber",
+      message: "What's the manager office number?",
+      validate: (nameInput) => {
+        if (isNaN(nameInput)) {
+          return false;
+        } else {
+          console.log("Please provide the office number");
+          return true;
+        }
+      },
+    },
+  ]);
 };
 
 //Engineer question
 const engineerQuestion = () => {
   return inquirer.prompt([
-  {
-    type: "input",
-    name: "username",
-    message: "What's the github user of the engineer office?",
-    validate: 
-  },
-  )];
+    {
+      type: "input",
+      name: "username",
+      message: "What's the github user of the engineer office?",
+      /* validate:  */
+    },
+  ]);
 };
 
 //Intern question
-const internQuestion = () =>{
-  return inquirer.prompt ([
-  {
-    type: "input",
-    name: "name",
-    message: "What's the school name of the intern?",
-  },
-  )];
+const internQuestion = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What's the school name of the intern?",
+    },
+  ]);
 };
 
 //In the case you need to add another employee
-const anotherQuestion = () =>{
-return inquirer.prompt ([
-  {
-    type: "list",
-    name: "name",
-    message: "Would you like to add another employee?",
-    choices: ["Yes", "No"],
-  },
-)];
+const anotherQuestion = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "name",
+      message: "Would you like to add another employee?",
+      choices: ["Yes", "No"],
+    },
+  ]);
 };
 
 //Create a function to blend together the data
+
 //Create a function to initialize the app
