c1e340c2b2d64a78d8c926c15949bcbb2cbdbfb9
Otilia Visan <otilia.visan22@gmail.com>
2021-05-14 00:30:54 +0100

    exported generateHTML

--- src/index.js
+++ src/index.js
@@ -8,156 +8,158 @@ const employees = [];
 
 const init = async () => {
   await addManager();
-//Questions
-while (!isTeam){
-const employeeQuestions = [
-    {
-      type: "list",
-      name: "employeeType",
-      message: "What type of employee needs to be added?",
-      choices: [
-    {name: "Engineer", value: "engineer", short: "Engineer"},
-    {name: "Intern", value: "intern", short: "Intern"},
-    {name: "None", value: "none", short: "None"},
-      ] 
+  //Questions
+  while (!isTeam) {
+    const employeeQuestions = [
+      {
+        type: "list",
+        name: "employeeType",
+        message: "What type of employee needs to be added?",
+        choices: [
+          { name: "Engineer", value: "engineer", short: "Engineer" },
+          { name: "Intern", value: "intern", short: "Intern" },
+          { name: "None", value: "none", short: "None" },
+        ]
       },
-]};
-const {employeeType} = await inquirer.prompt(employeeQuestions)
-if (employeeType ===none) {
-  isTeam = true;
-  console.log(employees);
-}else{
-  if(employeeType===engineer)
-  await addEngineer();
-}
-if (employeeType===intern) {
-  await addIntern();
-  
-};
+    ]
+  };
+  const { employeeType } = await inquirer.prompt(employeeQuestions)
+  if (employeeType === none) {
+    isTeam = true;
+    console.log(employees);
+  } else {
+    if (employeeType === engineer)
+      await addEngineer();
+  }
+  if (employeeType === intern) {
+    await addIntern();
+
+  };
 
 
-//create generateHTML function 
+  //create generateHTML function 
 
-const addManager = async() =>{
-  const managerQuestions = [
-   { type: "input",
-    name: "name",
-    message:"Please enter the manager name",
-    validate: validateInput,
-  },
+  const addManager = async () => {
+    const managerQuestions = [
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the manager name",
+        validate: validateInput,
+      },
 
-  {
-    type: "input",
-    name: "id",
-    message: "What's the id of your employee?",
-    validate: validateInput,
-  },
+      {
+        type: "input",
+        name: "id",
+        message: "What's the id of your employee?",
+        validate: validateInput,
+      },
 
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "Please enter your office number",
-    validate: validateInput,
-  },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter your office number",
+        validate: validateInput,
+      },
 
-  {
-    type: "input",
-    name: "email",
-    message: "Please enter your work email",
-    validate:validateInput,
-  },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter your work email",
+        validate: validateInput,
+      },
 
-  ];
+    ];
 
-  const managerAnswers = await inquirer.prompt(managerQuestion);
-const manager = new Manager(managerAnswers);
-employees.push(manager);
-}
-
-
-
-const addEngineer = async() =>{
-  const engineerQuestion = [
-
-    {
-      type:"input",
-      name: "name",
-      message: "Please enter the engineer name",
-      validate: validateInput,
-    },
-
-    {
-      type:"input",
-      name: "id",
-      message: "Please enter the engineer id",
-      validate: validateInput,
-    },
-
-    {
-      type:"input",
-      name: "email",
-      message: "Please enter the engineer email",
-      validate: validateInput,
-    },
-
-    {
-      type:"input",
-      name: "email",
-      message: "Please enter the engineer email",
-      validate: validateInput,
-    },
-
-    {
-      type:"input",
-      name: "github",
-      message: "Please enter the engineer github profile",
-      validate: validateInput,
-    },
-  ]
-  const engineerAnswers = await inquirer.prompt(engineerQuestions);
-console.log(engineerAnswers);
-const engineer = new Engineer(engineerAnswers);
-employees.push(engineer);
-
-};
+    const managerAnswers = await inquirer.prompt(managerQuestion);
+    const manager = new Manager(managerAnswers);
+    employees.push(manager);
+  }
 
 
 
-const addIntern = async() =>{
-  const internQuestions = [
-    {
-      type:"input",
-      name: "name",
-      message: "Please enter the intern name",
-      validate: validateInput,
-    },
+  const addEngineer = async () => {
+    const engineerQuestion = [
 
-    {
-      type:"input",
-      name: "id",
-      message: "Please enter the intern id",
-      validate: validateInput,
-    },
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the engineer name",
+        validate: validateInput,
+      },
 
-    {
-      type:"input",
-      name: "email",
-      message: "Please enter the intern email address",
-      validate: validateInput,
-    },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the engineer id",
+        validate: validateInput,
+      },
 
-    {
-      type:"input",
-      name: "school",
-      message: "Please enter the intern school name",
-      validate: validateInput,
-    },
-  ]
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the engineer email",
+        validate: validateInput,
+      },
 
-const internAnswers = await inquirer.prompt(internQuestions);
-console.log(internAnswers);
-const intern = newIntern(internAnswers);
-employees.push(intern);
-};
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the engineer email",
+        validate: validateInput,
+      },
 
-init();
+      {
+        type: "input",
+        name: "github",
+        message: "Please enter the engineer github profile",
+        validate: validateInput,
+      },
+    ]
+    const engineerAnswers = await inquirer.prompt(engineerQuestions);
+    console.log(engineerAnswers);
+    const engineer = new Engineer(engineerAnswers);
+    employees.push(engineer);
+
+  };
+
+
+
+  const addIntern = async () => {
+    const internQuestions = [
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the intern name",
+        validate: validateInput,
+      },
+
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the intern id",
+        validate: validateInput,
+      },
+
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the intern email address",
+        validate: validateInput,
+      },
+
+      {
+        type: "input",
+        name: "school",
+        message: "Please enter the intern school name",
+        validate: validateInput,
+      },
+    ];
+
+    const internAnswers = await inquirer.prompt(internQuestions);
+    console.log(internAnswers);
+    const intern = newIntern(internAnswers);
+    employees.push(intern);
+  };
+
+  init();
--- src/lib/generateHTML.js
+++ src/lib/generateHTML.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const path = require("path");
+const teamprofileDir = path.resolve(__dirname, "./teamprofile");
 
 const generateHTML = (employees) => {
   const HTML = [];
@@ -19,5 +21,52 @@ const generateHTML = (employees) => {
       .filter((intern) => intern.getRole() === "intern")
       .map((intern) => renderIntern(intern))
   );
-  return renderMarkdown(HTML.join(""));
+  return renderTeamProfile(HTML.join(""));
 };
+
+const renderManager = (manager) => {
+  let teamProfile = fs.readFileSync(
+    path.resolve(teamprofileDir, "teamprofile-html")
+  );
+  teamProfile = replaceTeamprofile(teamprofile, "name", manager.getName());
+  teamProfile = replaceTeamprofile(teamprofile, "id", manager.getId());
+  teamProfile = replaceTeamprofile(teamprofile, "email", manager.getEmail());
+  teamProfile = replaceTeamprofile(teamprofile, "role", manager.getRole());
+  teamProfile = replaceTeamprofile(
+    teamprofile,
+    "officeNumber",
+    manager.officeNumber()
+  );
+  return teamprofile;
+};
+
+const renderEngineer = (engineer) => {
+  let teamProfile = fs.readFileSync(
+    path.resolve(teamprofileDir, "teamprofile-html")
+  );
+  teamProfile = replaceTeamprofile(teamprofile, "name", engineer.getName());
+  teamProfile = replaceTeamprofile(teamprofile, "id", engineer.getId());
+  teamProfile = replaceTeamprofile(teamprofile, "email", engineer.getEmail());
+  teamProfile = replaceTeamprofile(teamprofile, "role", engineer.getRole());
+  teamProfile = replaceTeamprofile(teamprofile, "github", engineer.getGithub());
+  return teamprofile;
+};
+
+const renderIntern = (intern) => {
+  let teamProfile = fs.readFileSync(
+    path.resolve(teamprofileDir, "teamprofile-html")
+  );
+  teamProfile = replaceTeamprofile(teamprofile, "name", intern.getName());
+  teamProfile = replaceTeamprofile(teamprofile, "id", intern.getId());
+  teamProfile = replaceTeamprofile(teamprofile, "email", intern.getEmail());
+  teamProfile = replaceTeamprofile(teamprofile, "role", intern.getRole());
+  teamProfile = replaceTeamprofile(teamprofile, "school", engineer.getSchool());
+  return teamprofile;
+};
+
+const replaceTeamprofile = (teamprofile, placeholder, value) => {
+  const pattern = new RegExp(`{{${placeholder}}}`, "gm");
+  return teamprofile.replace(pattern, value);
+};
+
+module.export = generateHTML;
