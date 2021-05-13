7ba69d32267addd35c32f55d2d70f28ae9e2f452
Otilia Visan <otilia.visan22@gmail.com>
2021-05-13 23:31:28 +0100

    promp the questions/answers

--- src/index.js
+++ src/index.js
@@ -1,126 +1,163 @@
 //Include packages needed for this application
-const manager = require("./lib/manager");
-const engineer = require("./lib/engineer");
-const intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 const inquirer = require("inquirer");
 
-function init = () =>{
- questions();
- 
-}
+const employees = [];
+
+const init = async () => {
+  await addManager();
 //Questions
-
-const questions = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What's the name of your employee?",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please let me know the name of your employee");
-          return false;
-        }
-      },
-    },
-
-    {
-      type: "input",
-      name: "id",
-      message: "What's the id of your employee?",
-      validate: (nameId) => {
-        if (nameId) {
-          return true;
-        } else {
-          console.log("Please let me know the id of your employee");
-          return false;
-        }
-      },
-    },
-
-    {
-      type: "input",
-      name: "email",
-      message: "What's the email address of your employee?",
-      validate: (email) => {
-        valid =
-          /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
-            email
-          );
-        if (valid) {
-          return true;
-        } else {
-          console.log("Please enter your email address");
-          return false;
-        }
-      },
-    },
-
-    {
-      type: "input",
-      name: "list",
-      message: "What's the role of your employee?",
-      choices: ["manager", "intern", "engineer"],
-    },
-  ]);
-};
-
-// Manager question
-const managerQuestion = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "What's the manager office number?",
-      validate: (nameInput) => {
-        if (isNaN(nameInput)) {
-          return false;
-        } else {
-          console.log("Please provide the office number");
-          return true;
-        }
-      },
-    },
-  ]);
-};
-
-//Engineer question
-const engineerQuestion = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "username",
-      message: "What's the github user of the engineer office?",
-      /* validate:  */
-    },
-  ]);
-};
-
-//Intern question
-const internQuestion = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What's the school name of the intern?",
-    },
-  ]);
-};
-
-//In the case you need to add another employee
-const anotherQuestion = () => {
-  return inquirer.prompt([
+while (!isTeam){
+const employeeQuestions = [
     {
       type: "list",
-      name: "name",
-      message: "Would you like to add another employee?",
-      choices: ["Yes", "No"],
-    },
-  ]);
+      name: "employeeType",
+      message: "What type of employee needs to be added?",
+      choices: [
+    {name: "Engineer", value: "engineer", short: "Engineer"},
+    {name: "Intern", value: "intern", short: "Intern"},
+    {name: "None", value: "none", short: "None"},
+      ] 
+      },
+]};
+const {employeeType} = await inquirer.prompt(employeeQuestions)
+if (employeeType ===none) {
+  isTeam = true;
+  console.log(employees);
+}else{
+  if(employeeType===engineer)
+  await addEngineer();
+}
+if (employeeType===intern) {
+  await addIntern();
+  
 };
 
-//Create a function to blend together the data
 
-//Create a function to initialize the app
+//create generateHTML function 
+
+const addManager = async() =>{
+  const managerQuestions = [
+   { type: "input",
+    name: "name",
+    message:"Please enter the manager name",
+    validate: validateInput,
+  },
+
+  {
+    type: "input",
+    name: "id",
+    message: "What's the id of your employee?",
+    validate: validateInput,
+  },
+
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Please enter your office number",
+    validate: validateInput,
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your work email",
+    validate:validateInput,
+  },
+
+  ];
+
+  const managerAnswers = await inquirer.prompt(managerQuestion);
+const manager = new Manager(managerAnswers);
+employees.push(manager);
+}
+
+
+
+const addEngineer = async() =>{
+  const engineerQuestion = [
+
+    {
+      type:"input",
+      name: "name",
+      message: "Please enter the engineer name",
+      validate: validateInput,
+    },
+
+    {
+      type:"input",
+      name: "id",
+      message: "Please enter the engineer id",
+      validate: validateInput,
+    },
+
+    {
+      type:"input",
+      name: "email",
+      message: "Please enter the engineer email",
+      validate: validateInput,
+    },
+
+    {
+      type:"input",
+      name: "email",
+      message: "Please enter the engineer email",
+      validate: validateInput,
+    },
+
+    {
+      type:"input",
+      name: "github",
+      message: "Please enter the engineer github profile",
+      validate: validateInput,
+    },
+  ]
+  const engineerAnswers = await inquirer.prompt(engineerQuestions);
+console.log(engineerAnswers);
+const engineer = new Engineer(engineerAnswers);
+employees.push(engineer);
+
+};
+
+
+
+const addIntern = async() =>{
+  const internQuestions = [
+    {
+      type:"input",
+      name: "name",
+      message: "Please enter the intern name",
+      validate: validateInput,
+    },
+
+    {
+      type:"input",
+      name: "id",
+      message: "Please enter the intern id",
+      validate: validateInput,
+    },
+
+    {
+      type:"input",
+      name: "email",
+      message: "Please enter the intern email address",
+      validate: validateInput,
+    },
+
+    {
+      type:"input",
+      name: "school",
+      message: "Please enter the intern school name",
+      validate: validateInput,
+    },
+  ]
+
+const internAnswers = await inquirer.prompt(internQuestions);
+console.log(internAnswers);
+const intern = newIntern(internAnswers);
+employees.push(intern);
+};
+
+init();
