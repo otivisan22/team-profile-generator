bb2ed8ba1894c626eb11d58280d92aba23169995
Otilia Visan <otilia.visan22@gmail.com>
2021-05-14 20:59:52 +0100

    refactored the syntax for manager/intern classes

--- src/__tests__/intern.test.js
+++ src/__tests__/intern.test.js
@@ -2,20 +2,13 @@ const Intern = require("../lib/intern");
 
 describe("Intern", () => {
   describe("constructor tests", () => {
-    test("should construct intern object with name, id, email, github,school and role", () => {
-      const intern = new Intern(
-        "Sam",
-        222,
-        "sam22@gmail.com",
-        "https://github.com/samken22",
-        "Stavanger"
-      );
+    test("should construct intern object with name, id, email,school and role", () => {
+      const intern = new Intern("Sam", 222, "sam22@gmail.com", "Stavanger");
       expect(intern).toEqual({
         role: "Intern",
         name: "Sam",
         id: 222,
         email: "sam22@gmail.com",
-        github: "https://github.com/samken22",
         school: "Stavanger",
       });
     });
@@ -23,13 +16,7 @@ describe("Intern", () => {
 
   describe("method tests", () => {
     test("should return role when getRole is called", () => {
-      const intern = new Intern(
-        "Sam",
-        222,
-        "sam22@gmail.com",
-        "https://github.com/samken22",
-        "Stavanger"
-      );
+      const intern = new Intern("Sam", 222, "sam22@gmail.com", "Stavanger");
       expect(intern.getRole()).toEqual("Intern");
     });
   });
--- src/__tests__/manager.test.js
+++ src/__tests__/manager.test.js
@@ -2,50 +2,27 @@ const Manager = require("../lib/manager");
 
 describe("Manager", () => {
   describe("constructor tests", () => {
-    test("should construct manager object with name, id, email, github,school,role and officeNumber", () => {
-      const manager= new Manager(
-        "Sam",
-        222,
-        "sam22@gmail.com",
-        "https://github.com/samken22",
-        "Stavanger"
-        "2222222"
-      );
-      expect(intern).toEqual({
-        role: "Intern",
+    test("should construct manager object with name, id, email,role and officeNumber", () => {
+      const manager = new Manager("Sam", 222, "sam22@gmail.com", "2222222");
+      expect(manager).toEqual({
+        role: "Manager",
         name: "Sam",
         id: 222,
         email: "sam22@gmail.com",
-        github: "https://github.com/samken22",
-        school: "Stavanger",
-        officeNumber: "2222222"
+        officeNumber: "2222222",
       });
     });
   });
 
   describe("method tests", () => {
     test("should return role when getRole is called", () => {
-      const manager = new Manager(
-        "Sam",
-        222,
-        "sam22@gmail.com",
-        "https://github.com/samken22",
-        "Stavanger"
-        "2222222"
-      );
+      const manager = new Manager("Sam", 222, "sam22@gmail.com", "2222222");
       expect(manager.getRole()).toEqual("Manager");
     });
   });
 
   test("should return officeNumber when getOfficeNumber is called", () => {
-    const manager = new Manager(
-      "Sam",
-      222,
-      "sam22@gmail.com",
-      "https://github.com/samken22",
-      "Stavanger"
-      "2222222"
-    );
+    const manager = new Manager("Sam", 222, "sam22@gmail.com", "2222222");
     expect(manager.getOfficeNumber()).toEqual("2222222");
   });
 });
